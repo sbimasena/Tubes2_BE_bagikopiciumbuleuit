896f7c0bb30331bfe9effb0986d36c268ea3fae5
fithrarzk <167663689+fithrarzk@users.noreply.github.com>
2025-05-13 22:04:57 +0700

    feat: add docs

--- main.go
+++ main.go
@@ -416,7 +416,7 @@ func writeJSON(w http.ResponseWriter, data interface{}) {
 func withCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		if r.Method == "OPTIONS" {
